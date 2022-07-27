734b710f93687a6132169728fba949fd05ac5d08
Kirill Platonov <kirill@platonov.net>
2022-07-27 16:01:34 +0300

    Fix zeitwerk loading (#27)

--- app/channels/hotwire/livereload/reload_channel.rb
+++ app/channels/hotwire/livereload/reload_channel.rb
@@ -1,5 +1,3 @@
-return if Hotwire::Livereload::Engine.config.hotwire_livereload.reload_method == :turbo_stream
-
 class Hotwire::Livereload::ReloadChannel < ActionCable::Channel::Base
   def subscribed
     stream_from "hotwire-reload"
