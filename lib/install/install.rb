3cc128f5d89dd7deba90e82ca8881e0d3ebca38b
Kirill Platonov <kirill@platonov.net>
2024-12-21 22:43:45 +0100

    Dropped Redis dependency
    Starting from Rails 7.0 live reloading works fine with default `async` ActiveCable adapter

--- lib/install/install.rb
+++ lib/install/install.rb
@@ -12,19 +12,3 @@ else
   say %(  Add <%= hotwire_livereload_tags %> within the <head> tag in your custom layout.)
   say %(  If using `config.hotwire_livereload.reload_method = :turbo_stream`, place *after* the `<%= action_cable_meta_tag %>`.)
 end
-
-if CABLE_CONFIG_PATH.exist?
-  gemfile = File.read(Rails.root.join("Gemfile"))
-  if gemfile.include?("redis")
-    say "Uncomment redis in Gemfile"
-    uncomment_lines "Gemfile", %r{gem ['"]redis['"]}
-  else
-    say "Add redis to Gemfile"
-    gem "redis"
-  end
-
-  say "Switch development cable to use redis"
-  gsub_file CABLE_CONFIG_PATH.to_s, /development:\n\s+adapter: async/, "development:\n  adapter: redis\n  url: redis://localhost:6379/1"
-else
-  say 'ActionCable config file (config/cable.yml) is missing. Uncomment "gem \'redis\'" in your Gemfile and create config/cable.yml to use Hotwire Livereload.'
-end
